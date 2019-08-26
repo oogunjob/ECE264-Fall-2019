cc2f1f4ec2ab69e0483ae4340f9b0407efd31983
Oluwatosin Oloruntobi Ogunjobi <oogunjob@ecegrid-thin4.ecn.purdue.edu>
2019-08-26 13:36:55 -0400

    add new file

--- Part1/HW02Sort/sort.c
+++ Part1/HW02Sort/sort.c
@@ -26,7 +26,8 @@ bool checkOrder(int * arr, int size)
 
 // YOU MUST MODIFY THIS FUNCTION
 #ifdef TEST_SORT
-void ssort(int * arr, int size)
+void swap(int * num1, int * num2); // swaps two numbers
+void ssort(int * arr, int size) // selection sorting function
 {
   // This function has two levels of for
   // The first level specifies which element in the array
@@ -34,6 +35,33 @@ void ssort(int * arr, int size)
   // elements
 
   // After finding the smallest element among the unsorted elements,
-  // swap it with the element of the index from the first level 
+  // swap it with the element of the index from the first level
+  
+  int count; // counter variable for outer loop
+  int lcv; // counter variable for inner loop
+  int minimum; // minimum value in array loop
+  
+  for(count = 0; count < size; count++){
+	  minimum = count;
+	  for(lcv = count + 1; lcv < size; lcv++){
+		  if(arr[lcv] < arr[minimum])
+			  minimum = lcv;
+	  }
+	  if(count != minimum)
+		  swap(&arr[count], &arr[minimum]);
+  }
+  
+  return;
+}
+
+void swap(int * num1, int * num2)
+{
+  int temp; // temporary number
+  
+  temp = *num1;
+  *num1 = *num2;
+  *num2 = temp;
+  
+  return;
 }
 #endif

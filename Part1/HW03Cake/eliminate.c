b3c1f89967d36cfb9f3bbec2aa1c6e35d02ec5f4
Oluwatosin Oloruntobi Ogunjobi <oogunjob@ecegrid-thin4.ecn.purdue.edu>
2019-09-01 19:39:48 -0400

    working function

--- Part1/HW03Cake/eliminate.c
+++ Part1/HW03Cake/eliminate.c
@@ -20,24 +20,49 @@ void eliminate(int n, int k)
       fprintf(stderr, "malloc fail\n");
       return;
     }
+	
   // initialize all elements
-
-
+  int tally; // variable used to assign array values as 'A'
+  int count; // loop control variable for the outer loop
+  int count2; // loop control varibale for the inner loop
+  int remaining; // amount of students who have not been eliminated
   
-  // counting to k,
-  // mark the eliminated element
-  // print the index of the marked element
-  // repeat until only one element is unmarked
+  count2 = 1;
+  remaining = n;
+  
+  // initializes values of the array to alive
+  for(tally = 0; tally < n; tally++){
+    arr[tally] = 'A';
+  }
+  
+  // finds index values of all students that have been eliminated and marks them as 'X'
+  while(remaining > 1)
+  {
+    for(count = 0; count < n; count++) 
+    {
+      if(count2 % k == 0) // checks if the counter is an iteration of k
+      { 
+        if(arr[count] == 'A') // if the student is still in the game, he/she is eliminated
+        {			
+	      arr[count] = 'X'; //eliminates the student
+		  printf("%d\n", count); // prints index of eliminated student
+          remaining -= 1;
+          count2 = 1; // reinitializes the count to begin counting again from 1 	  
+		}
+	  }
+	  else if (arr[count] == 'A')
+		count2 += 1;
+	}
+  }
+  
+  //prints out the index of the remaining student
+  count = 0; //reinitializes count as 0
+  
+  while(arr[count] != 'A')
+	  count++;
+  
+   printf("%d\n", count); // the index of the student who gets the cake
 
-
-
-
-  // print the last one
-
-
-
-
-  // release the memory of the array
   free (arr);
 }
 #endif

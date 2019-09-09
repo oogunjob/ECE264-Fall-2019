19e128511b1c0d44013491d25aca9acbcbd9edac
Oluwatosin Oloruntobi Ogunjobi <oogunjob@ecegrid-thin4.ecn.purdue.edu>
2019-09-08 22:47:58 -0400

    Fixed spacing issue, need to fix newline

--- Part1/HW04File/filechar.c
+++ Part1/HW04File/filechar.c
@@ -44,12 +44,6 @@ void printCounts(int * counts, int size)
 		  printf("%d,  , %d\n", count, counts[count]);
   }
   
-  // print the values in counts in the following format
-  // each line has three items:
-  // ind, onechar, counts[ind]
-  // ind is between 0 and size - 1 (inclusive)
-  // onechar is printed if ind is between 'a' and 'z' or
-  // 'A' and 'Z'. Otherwise, print space
-  // if counts[ind] is zero, do not print
+  return;
 }
 #endif

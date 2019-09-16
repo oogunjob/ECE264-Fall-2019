9ebde643c08a7ccc758905ad953465d1a8b25a0d
Oluwatosin Oloruntobi Ogunjobi <oogunjob@ecegrid-thin4.ecn.purdue.edu>
2019-09-16 17:47:24 -0400

    working version after office hours

--- Part1/HW06Word/filestr.c
+++ Part1/HW06Word/filestr.c
@@ -9,10 +9,42 @@
 #ifdef TEST_COUNTWORD
 int countWord(char * filename, char * word, char * line, int size)
 {
+  //local variables
+  FILE * file; // storage variable for the file
+  int sum; // the amount of times that the word occurs
+  int count;
+
+  count = 0;  
+  file = fopen(filename, "r"); // opens up the file to be read
+  char * pointer; // return from strstr
+
+
+  if(file == NULL) // checks if the file can be opened or not
+  {
+    return -1;
+  }
+  
+  sum = 0; // initilizes value of sum as 0
+  
+  while(fgets(line, size, file) != NULL)
+  {
+   pointer = strstr(line, word);
+   while(pointer != NULL)
+   {
+    pointer += strlen(word); // sets the pointer equal to full length of the word
+    pointer = strstr(pointer, word);  // checks that the pointer is equal to word
+    count++; // increments amount of times word is found
+    }
+  }
+  
+  fclose(file);
+ 
+  return count;
   // filename: name of the input file
   // word: the word to search
   // line: temporary space to store what is read from the file
   // size: size of line
+  
   // open a file whose name is filename for reading
   // if fopen fails, return -1. 
   // if fopen succeeds, set sum to zero

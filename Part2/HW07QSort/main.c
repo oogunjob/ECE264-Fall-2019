fb4f29fb405f11fb03aaf419a74e122c50eb2849
Oluwatosin Oloruntobi Ogunjobi <oogunjob@ecegrid-thin4.ecn.purdue.edu>
2019-09-22 17:04:57 -0400

    several changes made

--- Part2/HW07QSort/main.c
+++ Part2/HW07QSort/main.c
@@ -15,45 +15,53 @@ int main(int argc, char * * argv)
   // argv[2]: name of output file
 
   // if argc is not 3, return EXIT_FAILURE
+  if(argc != 3)
+  {
+    return EXIT_FAILURE;
+  }
 
   // count the number of integers in the file
   int numElem = 0;
   numElem = countInt(argv[1]);
 
+  // checks if open fails or not
   if (numElem == -1) // fopen fails
     {
       return EXIT_FAILURE;
     }
 
-  // allocate memory for the integers in the file
-  // 1. create a pointer variable
-  // 2. allocate memory
-  // 3. check whether allocation succeed
-  //    if allocation fails, return EXIT_FAILURE
-
+  // allocation of memory for the array 
   int * intArr;
+  *intArr = malloc(sizeof(int) * numElem);
+  
+  //checks wheter allocation is succesfull or not
+  if(intArr == NULL)
+  {
+    return EXIT_FAILURE; // if the allocation was unsuccessful, EXIT_FAILURE is returned
+  }
 
+  
   bool rtv = readInt(argv[1], intArr, numElem);
 
   if (rtv == false) // read fail
     {
-  
+     return EXIT_FAILURE; 
     }
   
   // call qsort using the comparison function you write
-  qsort(....);
-
+  qsort(intArr, size, argv[1]);
+  
   // write the sorted array to a file whose name is argv[2]
   
   rtv = writeInt(argv[2], intArr, numElem);
   if (rtv == false) // read fail
     {
-      // release memory
+      free(intArr); // frees space
       return EXIT_FAILURE;
     }
 
-  // everything is ok, release memory, return EXIT_SUCCESS
-
+  free(intArr);
+  
   return EXIT_SUCCESS;
 }
 #endif

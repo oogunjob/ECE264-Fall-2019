ea54f44e893befd51d9d10d34d6f378df0c1e217
Oluwatosin Oloruntobi Ogunjobi <oogunjob@ecegrid-thin4.ecn.purdue.edu>
2019-09-26 12:44:46 -0400

    made comments simpler

--- Part2/HW08Struct/main.c
+++ Part2/HW08Struct/main.c
@@ -11,39 +11,63 @@
 #ifdef TEST_MAIN
 int main(int argc, char * * argv)
 {
+  //local variables
+  int vectorCount; // the number of vectors in the file
+  Vector * vectorArray; // array that holds space for the vectors
+  bool rtv; // boolean answer for validation of file success
+  
   // argv[1]: name of input file (binary)
   // argv[2]: name of output file (binary)
-
-  // check whether there are three arguments.
+  
+  //check whether there are three arguments.
   // If not, return EXIT_FAILURE. DO NOT print anything
-
+  if(argc != 3)
+  {
+    return EXIT_FAILURE;
+  }
+  
   // use argv[1] as the input to countVector, save the result
-
+  vectorCount = countVector(argv[1]);
+  
   // if the number of vector is 0 or negative, return EXIT_FAILURE
-
+  if(vectorCount <= 0)
+  {
+    return EXIT_FAILURE;
+  }
+  
   // otherwise, allocate memory for an array of vectors
-
+  vectorArray = malloc(sizeof(Vector) * vectorCount);
+  
+  
   // read the vectors from the file whose name is argv[1]. save the
   // results in the allocated array
   // if reading fails, release memory and return EXIT_FAILURE
+  rtv = readVector(argv[1], vectorArray, vectorCount);
+  
+  if(rtv == false)
+  {
+    free(vectorArray);
+    return EXIT_FAILURE;
+  }
 
 #ifdef DEBUG
   printVector(vecArr, numElem);
 #endif  
 
-  
-  // call qsort to sort the vectors, use argv[3] to determine which
-  // attribute to sort
 
 #ifdef DEBUG
   printf("\n");
-  printVector(vecArr, numElem);
+  printVector(vectorArray, numElem);
 #endif  
 
   // write the sorted array to the file whose name is argv[2]
   // if writing fails, release memory and return EXIT_FAILURE
   
-
-  // releave memory, return EXIT_SUCCESS
+  // NOT YET COMPLETED
+  
+ 
+  // release memory, return EXIT_SUCCESS
+  free(vectorArray);
+  return EXIT_SUCCESS;
 }
 #endif

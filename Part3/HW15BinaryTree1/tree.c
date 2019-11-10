9be976a43887ed7d7f131de690347e91264b25b7
Oluwatosin Oloruntobi Ogunjobi <oogunjob@ecegrid-thin4.ecn.purdue.edu>
2019-11-10 15:35:47 -0500

    passes testcases

--- Part3/HW15BinaryTree1/tree.c
+++ Part3/HW15BinaryTree1/tree.c
@@ -60,8 +60,69 @@ void preOrder(Tree * tr, char * filename)
 #ifdef TEST_BUILDTREE
 // Consider the algorithm posted on
 // https://www.geeksforgeeks.org/construct-a-binary-tree-from-postorder-and-inorder/
+TreeNode * buildStructure(int * inArray, int * postArray, int start, int end, int * index);
+int nodeSearch(int * array, int start, int value);
+TreeNode * newNode(int value);
 
 Tree * buildTree(int * inArray, int * postArray, int size)
 {
+  int index;
+  index = size - 1;
+  
+  Tree * X = malloc(sizeof(*X));
+  
+  X -> root = buildStructure(inArray, postArray, 0, size - 1, &index);
+  
+  return X;
+}  
+
+TreeNode * buildStructure(int * inArray, int * postArray, int start, int end, int * index)
+{
+  // starting test case, for if start is greater than the end
+  if(start > end)
+  {
+	return NULL;
+  }
+  
+  // creates new node using index
+  TreeNode * Node = newNode(postArray[*index]);
+  *index = *index - 1;
+  
+  // if the node doesn't have any children, return the node
+  if(start == end)
+  {
+	return Node;
+  }
+	
+  // finds the correct index
+  int nodeIndex = nodeSearch(inArray, start, Node -> value);
+  
+  Node -> right = buildStructure(inArray, postArray, nodeIndex + 1, end, index);
+  Node -> left = buildStructure(inArray, postArray, start, nodeIndex - 1, index);
+	
+  return Node;
 }
+
+int nodeSearch(int * array, int start, int value)
+{
+  int count; // counter variable for while loop
+  count = start;
+  
+  while(array[count] != value)
+  {
+    count++;
+  }	  
+
+  return count; // returns the index	
+}
+
+TreeNode * newNode(int value)
+{
+  TreeNode * Node = malloc(sizeof(*Node));
+  Node -> value = value;
+  Node -> right = NULL;
+  Node -> left = NULL;
+  
+  return Node;
+}	
 #endif
